ee4646d1fdc0ccb6b530699f5d8217afed4e3fd6
Susheel Yadav <sushyad@gmail.com>
2016-01-20 09:45:09 -0500

    removed the vacation changes for garage door check every 5 minutes

--- sanitize.sh
+++ sanitize.sh
@@ -1,1 +1,1 @@
-sed -e 's/0938ee[a-zA-Z0-9]*/CHANGE_ME/' rules/home.rules | sed -e 's/[a-zA-Z]*.[a-zA-Z]*@[a-zA-Z]*.com/abc@def.org/' | sed -e 's/[a-zA-Z]*_[a-zA-Z]*@[a-zA-Z]*.org/def@abc.org/'
+sed -e 's/0938ee[a-zA-Z0-9]*/CHANGE_ME/' rules/home.rules | sed -e 's/[a-zA-Z]*.[a-zA-Z]*@[a-zA-Z]*.com/abc@def.org/' | sed -e 's/[a-zA-Z]*_[a-zA-Z]*@[a-zA-Z]*.org/def@abc.org/' > rules/home.rules.new
